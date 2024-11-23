959c75f45baab7458e68619f32e4fa3953e663dc
miguelmaroune <miguelmaroune@gmail.com>
2024-11-23 14:45:31 +0100

    Initial commit for BankBuddy project

---
+++ src/test/java/com/sg/bankBuddy/bankBuddy_core/BankBuddyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.bankBuddy.bankBuddy_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBuddyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
